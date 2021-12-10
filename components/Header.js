a12eb1bcf5ba710d1b58bc16688e737b7da7b600
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-12-11 04:09:53 +0500

    user session working now can be used with useSess

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,9 @@ import {
   HeartIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon, MenuIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 export const Header = () => {
+  const { data: session } = useSession();
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
@@ -44,21 +46,28 @@ export const Header = () => {
         {/* right */}
         <div className="flex items-center justify-end space-x-5">
           <HomeIcon className="navBtn" />
-          <div className="relative navBtn">
-            <PaperAirplaneIcon className="navBtn rotate-45" />
-            <div className="absolute -top-1 -right-2 text-xs w-4 h-4 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
-              3
-            </div>
-          </div>
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <PlusCircleIcon className="navBtn" />
-          <UserGroupIcon className="navBtn" />
-          <HeartIcon className="navBtn" />
-          <img
-            className="h-10 cursor-pointer rounded-full"
-            src="https://media-exp1.licdn.com/dms/image/C4E03AQH5_54XQXJtPw/profile-displayphoto-shrink_800_800/0/1606752728327?e=1644451200&v=beta&t=BW7rZoo911IXAr483ZEWDjs-dsdYS5q6Y692QVvL2jM"
-            alt="profile pic"
-          />
+          {session ? (
+            <>
+              <div className="relative navBtn">
+                <PaperAirplaneIcon className="navBtn rotate-45" />
+                <div className="absolute -top-1 -right-2 text-xs w-4 h-4 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                  3
+                </div>
+              </div>
+              <PlusCircleIcon className="navBtn" />
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
+              <img
+                onClick={signOut}
+                className="h-10 cursor-pointer rounded-full"
+                src={session.user?.image}
+                alt="profile pic"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,5 +1,26 @@
-function signIn() {
-  return <div>i am the Sign in page</div>;
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+
+function signIn({ providers }) {
+  console.log(process.env.GOOGLE_CLIENT_ID);
+  console.log(process.env.GOOGLE_CLIENT_SECRET);
+  return (
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => SignIntoProvider(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
 }
 
 export default signIn;
